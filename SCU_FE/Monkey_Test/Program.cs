ffe5685b97808bbd730ddff59da2b3b4623a22cf
jayhsieh <jay.cc.hsieh@gmail.com>
2016-05-19 22:50:13 +0800

    add WELL random generator

---
+++ SCU_FE/Monkey_Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using Utility;
+using System.IO;
+namespace Monkey_Test {
+    class Program {
+        static void Main(string[] args) {
+            RandomWELL rnd = new RandomWELL();
+            FileStream rndWELL = new FileStream("random_WELL.txt", System.IO.FileMode.OpenOrCreate);
+            StreamWriter bw = new StreamWriter(rndWELL);
+            rnd.srand(7654321);
+            for (int i = 0; i < 256*256; i++) {
+                bw.WriteLine(rnd.frand2());
+            }
+            bw.Flush();
+            bw.Close();
+        }
+    }
+}
