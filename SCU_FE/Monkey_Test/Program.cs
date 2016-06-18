5f69deb9aad3289da575ea264e8ac8496d67f7ed
jayhsieh <jay.cc.hsieh@gmail.com>
2016-06-18 14:44:20 +0800

    new test

--- SCU_FE/Monkey_Test/Program.cs
+++ SCU_FE/Monkey_Test/Program.cs
@@ -1,17 +1,27 @@
 ﻿using Utility;
 using System.IO;
+using System;
+using Simulation;
 namespace Monkey_Test {
     class Program {
         static void Main(string[] args) {
             RandomWELL rnd = new RandomWELL();
+            Normal01 n01 = new Normal01();
+            Random qqq = new Random();
             FileStream rndWELL = new FileStream("random_WELL.txt", System.IO.FileMode.OpenOrCreate);
             StreamWriter bw = new StreamWriter(rndWELL);
-            rnd.srand(7654321);
-            for (int i = 0; i < 256*256; i++) {
-                bw.WriteLine(rnd.frand2());
+            
+            for (int i = 0; i < 256 * 256; i++) {
+                bw.WriteLine(n01.NextGaussianDouble_Box_Muller());
+                //Console.WriteLine(rnd.frand2());
             }
             bw.Flush();
             bw.Close();
+        }
+
+        static void Main_111(string[] args) {
+            Heston hh = new Heston();
+            hh.HestonMC(100, 100, 0, 1, 0, 2, 0.01, 0, 0.1, 0.01, 5000);   
         }
     }
 }
