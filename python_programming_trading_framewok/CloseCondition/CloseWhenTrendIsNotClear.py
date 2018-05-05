92d3fb1897370583dc73cb4f53ac0f5d1e8a665b
Jay Hsieh <jay.cc.hsieh@gmail.com>
2018-05-05 12:39:23 +0800

    Add files via upload

---
+++ python_programming_trading_framewok/CloseCondition/CloseWhenTrendIsNotClear.py
@@ -0,0 +1,45 @@
+'''
+Created on 2018年4月30日
+
+@author: Albert
+'''
+from RealTime.function import getOrder
+
+
+
+#取得報價資訊，詳情請查看技巧51
+with open('function.py','r') as f:
+	exec(f.read())
+
+#定義指標變數
+lastBAmount=0
+lastSAmount=0
+
+#假設目前倉位為買方，進場部分請參考第七章
+index=1
+orderPrice=10300
+
+#取得委託資訊
+for i in getOrder():		
+	OrderInfo=i.split(',')
+	OrderBAmount=int(OrderInfo[2])
+	OrderSAmount=int(OrderInfo[4])
+
+	if lastBAmount==0 and lastSAmount==0:
+		lastBAmount=OrderBAmount
+		lastSAmount=OrderSAmount
+
+	diffBAmount=OrderBAmount-lastBAmount
+	diffSAmount=OrderSAmount-lastSAmount
+
+	#抽單出場判斷
+	if index==1:
+		if diffBAmount <= -100:
+			index=0
+			print( MatchInfo[0],"Order Sell Success!")
+			break
+	elif index==-1:
+		if diffSAmount <= -100:
+			index=0
+			print( MatchInfo[0],"Order Buy Success!")
+			break
